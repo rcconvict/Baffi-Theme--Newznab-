196055bf0188e33b3061a3c275841bd78d03b135
Fredrik Soløy <fredrik.soloy@gmail.com>
2013-01-09 13:54:09 +0100

    Made a install script

---
+++ runme.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+set -e
+clear
+echo " "
+echo " 1. Install baffi:theme "
+echo " 2. Remove baffi:theme "
+echo " 3. Update baffi:theme [Not yet implemented]"
+echo " "
+echo -n " What do you want? "
+read WISH0
+
+if [ $WISH0 = "1" ] ; then
+	clear
+	echo " "
+	echo -n " Is this folder located in the root of the newznab folder? [y/n] "
+	read WISH1
+	if [ $WISH1 = "n" ] ; then
+		echo " Please put it in the ../newznab directory."
+		exit
+	fi
+	
+	echo " "
+	echo " Copying the Baffi:Theme"
+	cp -r baffi ../www/views/themes/baffi
+	
+	cp bootstrap.js ../www/views/scripts/bootstrap.js
+	
+	clear
+	echo " "
+	echo -n " Go and select the Baffi theme in admin->site edit. Have you done it? [y/n] "
+	read WISH12
+	if [ $WISH12 = "y" ] ; then
+	
+		rm -r ../www/lib/smarty/templates_c/*
+	
+		echo " "
+		echo " Copying the Baffi:Templates"
+		cp -r templates_baffi ../www/views/templates_baffi
+		
+		echo " "
+		echo " Backuping the basepage"
+		mv ../www/lib/framework/basepage.php ../www/lib/framework/basepage_old.php 
+
+		echo " "
+		echo " Feeding the Baffi:basepage"
+		cp basepage.php ../www/lib/framework/basepage.php 
+	
+	fi
+
+	echo " "
+	echo " I think we are about done for now."
+	echo " "
+fi
+
+
+if [ $WISH0 = "2" ] ; then
+	clear
+	echo " "
+	echo -n " Have you choosed another theme in the admin->site edit? [y/n] "
+	read WISH2
+	if [ $WISH2 = "n" ] ; then
+		echo " Please do so first"
+		exit
+	fi
+	
+	echo " "
+	echo " Removing the Baffi:Theme"
+	rm -r ../www/views/themes/baffi
+	rm ../www/views/scripts/bootstrap.js
+	
+	echo " "
+	echo " Removing the Baffi:Templates"
+	rm -r ../www/views/templates_baffi
+
+	echo " "
+	echo " Removing the Baffi:basepage"
+	rm ../www/lib/framework/basepage.php 
+
+	echo " "
+	echo " Reverting back to the backup of the basepage"
+	mv ../www/lib/framework/basepage_old.php ../www/lib/framework/basepage.php 
+
+	echo " "
+	echo " Reseting the cache"
+	rm -r ../www/lib/smarty/templates_c/*
+	
+	echo " "
+	echo " I think we are about done for now."
+	echo " "
+fi
+
+exit

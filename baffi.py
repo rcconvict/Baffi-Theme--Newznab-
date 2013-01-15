34a68405b0c779cff1893f623c29a63be68f31a2
convict <convict@jailcell.org>
2013-01-14 19:09:25 -0500

    Running update now checks how many commits behind the user is.

--- baffi.py
+++ baffi.py
@@ -3,6 +3,61 @@
 # Written by convict
 
 import sys, os, shutil
+import platform, subprocess, re, os, json, urllib2
+
+def runGit(args):
+	git_locations = ['git']
+	run_dir = os.getcwd()
+	
+	if platform.system().lower() == 'darwin':
+		git_locations.append('/usr/local/git/bin/git')
+	
+	output = err = None
+	for cur_git in git_locations:
+		cmd = cur_git + ' ' + args
+		try:
+			p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, cwd=run_dir)
+			output, err = p.communicate()
+		except OSError:
+			print 'Command "%s" did not work. Could not find git.' % cmd
+			continue
+	
+		if 'not found' in output or 'not recognized as an internal or external command' in output:
+			print 'Unable to find git with command "%s"' % cmd
+			output = None
+		elif 'fatal:' in output or err:
+			print 'Git returned bad info. Are you sure this is a git installation?'
+			output = None
+		elif output:
+			break
+	return (output, err)
+
+def gitCurrentVersion():
+	output, err = runGit('rev-parse HEAD')
+
+	if not output:
+		print 'Could not find latest installed version with git.'
+		return None
+
+	current_commit = output.strip()
+
+	if not re.match('^[a-z0-9]+$', current_commit):
+		print 'Git output does not look like a hash, not using it.'
+		return None
+
+	return current_commit
+
+def latestCommit():
+	url = 'https://api.github.com/repos/Frikish/Baffi-Theme--Newznab-/commits/master'
+	result = urllib2.urlopen(url).read()
+	git = json.JSONDecoder().decode(result)
+	return git['sha']
+
+def commitsBehind():
+	url = 'https://api.github.com/repos/Frikish/Baffi-Theme--Newznab-/compare/%s...%s' % (gitCurrentVersion(), latestCommit())
+	result = urllib2.urlopen(url).read()
+	git = json.JSONDecoder().decode(result)
+	return git['total_commits']
 
 def install(update=False):
 	# copy theme folders
@@ -17,6 +72,7 @@ def install(update=False):
 	# if this is not an update then backup pre-existing files
 	if update == False:
 		raw_input('Go and select the baffi theme in admin->site edit. Press enter to continue...')
+
 	# make sure backups are in place
 	try:
 		if not os.path.isfile('../www/views/scripts/utils_old_original.js'):
@@ -30,6 +86,8 @@ def install(update=False):
 			os.rename('../www/views/scripts/utils-admin.js', '../www/views/scripts/utils-admin_old.js')
 	except OSError, e:
 		print 'Error copying/renaming frameworks: %s' % e
+	except IOError, e:
+		sys.exit('ERROR! Files are missing. Go to your newznab folder and run "svn up" to restore them. %s' % e)
 
 	# copy files to www/views/scripts
 	files = ['bootstrap.js', 'utils.js', 'jquery.pnotify.js', 'utils-admin.js']
@@ -109,6 +167,9 @@ def main(switch):
 		uninstall()
 		print 'Uninstall finished.'
 	if switch == 'update':
+		comm = commitsBehind()
+		if comm > 0:
+			print 'You are %s commits behind. It\'s recommended to run "git pull" then re-run python baffi.py update' % comm
 		uninstall(True)
 		install(True)
 		print 'Update finished.'
